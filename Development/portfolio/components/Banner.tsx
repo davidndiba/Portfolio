ddb4900a04ffe94f1e3a4911915080d8641c9962
David <david.ndiba@student.moringaschool.com>
2023-12-08 08:06:01 +0300

    modified banner

--- Development/portfolio/components/Banner.tsx
+++ Development/portfolio/components/Banner.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     <section
       id="home"
-      className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
+      className="max-w-[800px] mx-auto mt-4 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
     >
       <motion.h2
         initial={{ y: 10, opacity: 0 }}
--- Development/portfolio/pages/index.tsx
+++ Development/portfolio/pages/index.tsx
@@ -15,42 +15,34 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll'>
-      <Navbar/>
-      <div className='w-full h-[88vh] xl!flex items-center gap-20 justify-between'>
+      <Navbar />
+      <div className='flex flex-col xl:flex-row items-center gap-20 justify-center xl:justify-between h-[88vh]'>
         <motion.div 
-        initial={{ opacity: 0}}
-        animate={{ opacity: 1}}
-        transition={{ delay: 1.5}}
-        className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{ delay: 1.5}}
+          className='xl:hidden w-full mb-4'
         >
-          <LeftSide/>
-          </motion.div> 
-        </div>
-        <div className='h-[88vh] mx-auto p-4'>
+          <LeftSide />
+        </motion.div>
+        <div className='w-full xl:w-[800px] mx-auto p-4'>
           <Banner />
-           <About/>
-          <Skills/>
-          <Projects/>
-          <Archive/>
-          <Contact/>
-          <Footer/>
+          <About />
+          <Skills />
+          <Projects />
+          <Archive />
+          <Contact />
+          <Footer />
         </div>
         <motion.div 
-        initial={{ opacity: 0}}
-        animate={{ opacity: 1}}
-        transition={{ delay: 1.5}}
-        className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{ delay: 1.5}}
+          className='xl:hidden w-full mt-4'
         >
-        
-          </motion.div> 
-        <motion.div 
-        initial={{ opacity: 0}}
-        animate={{ opacity: 1}}
-        transition={{ delay: 1.5}}
-        className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'
-        >
-          <RightSide/>
-          </motion.div> 
+          <RightSide />
+        </motion.div>
+      </div>
     </main>
   )
 }
